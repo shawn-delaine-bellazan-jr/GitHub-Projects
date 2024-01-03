808b1927dc893b4904e3ae05d717d29d8abe59c6
Solo <shawn_delaine_bellazan_jr@hotmail.com>
2024-01-03 16:46:52 -0600

    Adding clean Architecture
    This structure promotes a clear separation of concerns and allows each layer to evolve independently, making it easier to maintain, test, and extend the application. Note that this is a simplified example, and you may need to adapt it based on the specific requirements of your project.

---
+++ io.github.shawn-delaine-bellazan-jr/ProjectHub.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ProjectHub.Domain
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ io.github.shawn-delaine-bellazan-jr/ProjectHub.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ProjectHub.Infrastructure
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
